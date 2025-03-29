b5814b398707d9a0a722c8a7efcc4874c186fd3f
b00815093 <imene.ousser@essec.edu>
2025-03-29 23:01:10 +0100

    Create ProductCard.js

---
+++ src/ProductCard.js
@@ -0,0 +1,25 @@
+function ProductCard({ product }) {
+	return (
+		<div className="product-card">
+			<h2>{product.title.en}</h2>
+			<p>{product.description.en}</p>
+			<ul>
+				{/* Render the benefits list */}
+				{product.benefits.map((benefit, index) => (
+					<li key={index}>
+						<span>{benefit.icon}</span> {benefit.text}
+					</li>
+				))}
+			</ul>
+			<p>Weight: {product.weight}</p>
+			<p>Loyalty Points: {product.loyaltyPoints}</p>
+			<p>{product.shippingInfo}</p>
+			{/* Render the call-to-action section */}
+			<button>{product.callToAction.buttonText}</button>
+			<p>{product.callToAction.inStock}</p>
+			<label>{product.callToAction.quantityLabel}</label>
+		</div>
+	);
+}
+
+export default ProductCard;
